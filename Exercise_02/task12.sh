5f94f39dbd8bc8555487ad31380f870d05f7123f
Stoyan Zlatev <tonkatazl@gmail.com>
2023-03-05 17:26:38 +0200

    Add exercise 02 tasks

---
+++ Exercise_02/task12.sh
@@ -0,0 +1,7 @@
+#Изтрийте файловете в home директорията си по-нови от practice/01/f3
+#(подайте на rm опция -i за да може да изберете само тези които искате да изтриете).
+
+find ~ -newer practice/01/f3 -type f -exec rm {} \;
+#or
+find ~ -newer practice/01/f3 -type f -delete
+#second way it does not ask for confirmation to delete
