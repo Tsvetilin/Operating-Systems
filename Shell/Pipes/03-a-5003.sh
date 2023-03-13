b2f0f7f3de6dda536f5661c935479f215e20b687
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-03-13 11:40:18 +0200

    Update 03-a-5003.sh

--- Shell/Pipes/03-a-5003.sh
+++ Shell/Pipes/03-a-5003.sh
@@ -1,3 +1,3 @@
 #Изведете имената на хората с второ име по-късо от 8 (<=7) символа според /etc/passwd 
 
-cat /etc/passwd | cut -d ':' -f 5 | cut -d ' ' -f 2 | cut -d ',' -f 1 | grep -v -E '.{8,}'
+cat /etc/passwd | cut -d ':' -f 5 | cut -d ',' -f1 | cut -d ' ' -f2 | grep -v -E '.{8,}$' | grep -E '[^a-zA-Z]' | sort | uniq | xargs -I{} grep -w {} /etc/passwd | wc -l
