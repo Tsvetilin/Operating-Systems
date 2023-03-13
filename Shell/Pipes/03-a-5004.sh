9ba8e61681eadbeecfea03aaac8cc96f8cf68b89
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-03-13 11:41:11 +0200

    Update 03-a-5004.sh

--- Shell/Pipes/03-a-5004.sh
+++ Shell/Pipes/03-a-5004.sh
@@ -1,3 +1,2 @@
 #Изведете целите редове от /etc/passwd за хората от 03-a-5003
-
-cat /etc/passwd | cut -d ':' -f 5 | cut -d ' ' -f 2 | cut -d ',' -f 1 | grep -v -E '.{8,}' | xargs -I{} grep {} /etc/passwd
+cat /etc/passwd | cut -d ':' -f5 | cut -d ',' -f1 | cut -d ' ' -f2 | grep -v -E '.{8,}$' | grep -E '[^a-zA-Z]' | sort | uniq | xargs -I{} grep -w {} /etc/passwd 
