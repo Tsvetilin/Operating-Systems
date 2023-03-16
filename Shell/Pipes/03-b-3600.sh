b69f8fe95883403aa701b9a73ba952e1fcaa101e
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-16 18:37:32 +0200

    Update pipe task

--- Shell/Pipes/03-b-3600.sh
+++ Shell/Pipes/03-b-3600.sh
@@ -1,4 +1,4 @@
 #Направете списък с директориите на вашата файлова система, до които нямате достъп.
 #Понеже файловата система може да е много голяма, търсете до 3 нива на дълбочина.
 
-find / -maxdepth 3 -type d \( ! -user $(whoami) -o ! -group $(id -gn) \) 2>/dev/null
+find / -maxdepth 3 -type d >/dev/null 2> sol.txt; cat sol.txt | cut -d ':' -f 2 | tr -d ' ' | cut -c 4- | rev | cut -c 4- | rev; rm sol.txt
