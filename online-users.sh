b2f276c36c0750c7608101973c80e02afdd32c23
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-16 16:49:12 +0200

    Update alias

--- online-users.sh
+++ online-users.sh
@@ -1,1 +1,8 @@
 who | cut -d ' ' -f 1 | xargs -I{} grep {} /etc/passwd | cut -d ':' -f 1,5 |cut -d ',' -f 1 | sort | uniq | column -t -s ':'
+
+
+#Save as alias
+alias onlineUsers="who | cut -d ' ' -f 1 | xargs -I{} grep {} /etc/passwd | cut -d ':' -f 1,5 |cut -d ',' -f 1 | sort | uniq | column -t
+-s ':' "
+#call
+onlineUsers
