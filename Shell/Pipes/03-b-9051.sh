c8224b00ca746b4c68507e5f36919664c7b8fb7f
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-27 09:30:26 +0300

    Update task

--- Shell/Pipes/03-b-9051.sh
+++ Shell/Pipes/03-b-9051.sh
@@ -7,3 +7,7 @@ cat population.csv | egrep ",2016," | awk -F ',' '{sum += $NF} END{print sum}'
 #or
 
 cat population.csv | awk -F ',' 'BEGIN {counter = 0} $3==2008 {counter+=$4} END {print counter}'
+
+#or
+
+cat population.csv | egrep ",2008," | cut -d ',' -f 4 | sed 's/.*/+ &/g' | xargs | tail -c +3 | bc
