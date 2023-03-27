4693839c2ff12a1e8e697ef7a48f2a8b76a2253b
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-27 10:41:43 +0300

    Update pipe task

--- Shell/Pipes/03-b-9102.sh
+++ Shell/Pipes/03-b-9102.sh
@@ -1,3 +1,7 @@
 #Да се изведат само имената на песните.
 
-find songs -mindepth 1| cut -d '-' -f2 | cut -c 2- | cut -d '(' -f1
+find songs -mindepth 1 | cut -d '-' -f2 | cut -c 2- | cut -d '(' -f1
+
+#or
+
+find songs -iname '*.ogg' | sed -E 's/.*- (.*)\(.*/\1/g'
