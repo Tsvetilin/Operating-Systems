664dd839a1637965db2cf34fb59a016ae28df189
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-27 09:24:54 +0300

    Update pipe task

--- Shell/Pipes/03-b-9051.sh
+++ Shell/Pipes/03-b-9051.sh
@@ -2,7 +2,8 @@
 #през 2008 година. А през 2016?
 
 cat population.csv | egrep ",2008," | awk -F ',' '{sum += $NF} END{print sum}'
+cat population.csv | egrep ",2016," | awk -F ',' '{sum += $NF} END{print sum}'
 
 #or
 
-cat population.csv | egrep ",2016," | awk -F ',' '{sum += $NF} END{print sum}'
+cat population.csv | awk -F ',' 'BEGIN {counter = 0} $3==2008 {counter+=$4} END {print counter}'
