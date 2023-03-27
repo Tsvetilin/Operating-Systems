d66f4b1f60b0ab95cdc27663dd6e1d533e2f3f29
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-03-27 09:20:22 +0300

    Update 03-b-9054.sh

--- Shell/Pipes/03-b-9054.sh
+++ Shell/Pipes/03-b-9054.sh
@@ -1,4 +1,4 @@
 #Използвайки файл population.csv, намерете коя държава е на 42-ро място по
 #население през 1969. Колко е населението й през тази година?
 
-cat population.csv | egrep ",1969," | awk -F ',' '{print $0 ": " $(NF)}' | sort -t ':' -k2n | cut -d ':' -f1 | tail -n 1| awk -F ',' '{for (i=1; i< (NF-2);i+=1) printf "%s,", $i} {print " " $(NF)}'
+cat population.csv | egrep ",1969," | awk -F ',' '{print $0 ": " $(NF)}' | sort -t ':' -k2n | cut -d ':' -f1 |head -n 42 | tail -n 1| awk -F ',' '{for (i=1; i< (NF-2);i+=1) printf "%s,", $i} {print " " $(NF)}'
