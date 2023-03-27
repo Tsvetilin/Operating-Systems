b5915a21d8431ac229c07f6a62c69101d0b1345a
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-27 09:41:43 +0300

    Update

--- Shell/Pipes/03-b-9052.sh
+++ Shell/Pipes/03-b-9052.sh
@@ -1,3 +1,11 @@
 # Използвайки файл population.csv, намерете през коя година в България има най-много население.
 
 grep "BGR" population.csv | sort -k4nr -t ',' | head -n 1 |cut -d ',' -f3
+
+#or
+
+cat population.csv | egrep 'Bulgaria' | sort -r -n -k 4 -t ',' | head -n 1 | cut -d ',' -f 3
+
+#or
+
+cat population.csv | awk -F ',' 'BEGIN{year=0; max=0} $(NF-2)=="BGR"{if(max<$(NF)){max=$NF; year=$(NF-1)}} END{print year}'
