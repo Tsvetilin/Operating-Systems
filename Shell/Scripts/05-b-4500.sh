fb03781556b1d178a9ddc84623123365be37bd64
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-04-06 12:26:37 +0300

    Add scripts tasks

---
+++ Shell/Scripts/05-b-4500.sh
@@ -0,0 +1,30 @@
+#Да се напише shell скрипт, който получава при стартиране като параметър в командния ред идентификатор на потребител. Скриптът периодично (sleep(1)) да проверява дали потребителят е log-нат, и ако да - да прекратява изпълнението си, извеждайки на стандартния изход подходящо съобщение.
+
+#!/bin/bash
+
+if [[ ${#} -ne 1 ]] ; then
+        echo "Expected 1 argument - user id"
+        exit 1
+fi
+
+if ! ( cat /etc/passwd | grep -q "${1}" ) ; then
+        echo "User ${1} does not exists"
+        exit 2
+fi
+# or
+#
+#if ! id "${1}" &> /dev/null ; then
+#        echo "Invalid user id"
+#        exit 2
+#fi
+
+echo "Waiting for user ${1} to log in"
+
+while true ; do
+        if who | grep -q "${1}" ; then
+                echo "User ${1} is logged in"
+                exit 0
+        fi
+
+        sleep 1
+done
---
+++ Shell/Scripts/05-b-4700.sh
@@ -0,0 +1,31 @@
+#Да се напише shell скрипт, който форматира големи числа, за да са по-лесни за четене.
+#Като пръв аргумент на скрипта се подава цяло число.
+#Като втори незадължителен аргумент се подава разделител. По подразбиране цифрите се разделят с празен интервал.
+
+#!/bin/bash
+
+separator=' '
+
+if [[ ${#} -lt 1 ]] || [[ ${#} -gt 2 ]] ; then
+        echo "Expected #1 argument - number"
+        echo "Optional #2 argument - separator"
+        exit 1
+fi
+
+if [[ ${#} -eq 2 ]] ; then
+
+    if cat ${2} | grep -E -q '^.$' ; then
+        separator=${2}
+    else
+        echo "Single character separator expected"
+        exit 2
+    fi
+fi
+
+if ! ( cat ${1} | grep -q -E '^(-){0,1}[0-9]+$' ) ; then
+        echo "Invalid number"
+        exit 2
+fi
+
+echo $(cat "${1}" | rev | sed -E 's/(.)/\1\n/g' | awk -v separator="${separator}" 'BEGIN {counter = 0;} {counter++; printf $0} co
+unter%3==0{printf separator}' | rev )
