d4497f5b613e8cb220e1dc4c818e0eadc8c623db
Stoyan Zlatev <tonkatazl@gmail.com>
2023-04-14 22:46:14 +0300

    Add scripts

--- Shell/Scripts/05-b-3200.sh
+++ Shell/Scripts/05-b-3200.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#v1
 if [[ ${#} -ne 1 ]] ; then
         echo "Expected 1 argument - folder path"
         exit 1
@@ -17,5 +18,20 @@ directoriesCount=$(find "${1}" -mindepth 1 -type d 2>&1 | wc -l)
 echo "Files: ${filesCount}"
 echo "Directories: ${directoriesCount}"
 echo Total count: $((filesCount + directoriesCount))
+
+#v2
+
+read -p "Enter directory name: " dirName
+
+if [[ ! -d ${dirName} ]] ; then
+        echo "Directory does not exist"
+        exit 1
+fi
+
+filesCount=$(find "${dirName}" -mindepth 1 -type f | wc -l)
+directoriesCount=$(find "${dirName}" -mindepth 1 -type d | wc -l)
+
+echo "Files: ${filesCount}"
+echo "Directories: ${directoriesCount}"
 
 #Да се напише shell скрипт, който приканва потребителя да въведе пълното име на директория и извежда на стандартния изход подходящо съобщение за броя на всички файлове и всички директории в нея.
---
+++ Shell/Scripts/05-b-3400.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]] ; then
+        echo "Expected 2 arguments"
+        exit 1
+fi
+
+fileName=$1
+shift
+
+word=$@
+
+grep -q "${word}" ${fileName}
+status=$?
+
+if [[ ${status} == 0 ]] ; then
+        echo "${fileName} contains ${word}"
+else
+        echo "${fileName} does not contain ${word}"
+fi
+
+#Да се напише shell скрипт, който чете от стандартния вход име на файл и символен низ, проверява дали низа се съдържа във файла и извежда на стандартния изход кода на завършване на командата с която сте проверили наличието на низа.
+#
+#NB! Символният низ може да съдържа интервал (' ') в себе си.
