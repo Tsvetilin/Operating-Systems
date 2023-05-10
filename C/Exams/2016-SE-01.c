f6ac8489088a1068d1ac0bc01f171afb37c0da91
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-05-10 17:56:25 +0300

    Update

--- C/Exams/2016-SE-01.c
+++ C/Exams/2016-SE-01.c
@@ -7,32 +7,48 @@
 
 int main(int argc, char **argv)
 {
+        if (argc != 2)
+        {
+                errx(1, "Invalid number of arguments! Usage: %s <file_name>", argv[0]);
+        }
+
         char *file_name = argv[1];
 
-        int fd = open(file_name, O_RDWR);
+        int fd;
 
-        if (fd < 0)
+        if ((fd = open(file_name, O_RDWR)) < 0)
         {
                 err(2, "Cannot open file %s", file_name);
         }
 
-        int bytes[256] = {0};
+        uint32_t bytes[256] = {0};
 
         uint8_t buf;
-
-        while (read(fd, &buf, sizeof(buf)) > 0)
+        int bytes_read = 0;
+        while ((bytes_read = read(fd, &buf, sizeof(buf))) > 0)
         {
                 bytes[buf] += 1;
         }
 
-        lseek(fd, 0, SEEK_SET);
+        if (bytes_read < 0)
+        {
+                err(3, "Cannot read from file %s", file_name);
+        }
+
+        if (lseek(fd, 0, SEEK_SET) < 0)
+        {
+                err(4, "Cannot lseek file %s", file_name);
+        }
 
         for (int i = 0; i < 256; i++)
         {
                 uint8_t c = i;
                 for (int j = 0; j < bytes[i]; j++)
                 {
-                        write(fd, &c, sizeof(c));
+                        if (write(fd, &c, sizeof(c)) < 0)
+                        {
+                                err(5, "Cannot write to file %s", file_name);
+                        }
                 }
         }
 
