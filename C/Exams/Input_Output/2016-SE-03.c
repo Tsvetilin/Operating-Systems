f19587a2ed0403b80caed0ab1505f45c99070d34
Stoyan Zlatev <tonkatazl@gmail.com>
2023-06-17 15:24:30 +0300

    Add C Exam Task Solution

--- C/Exams/Input_Output/2016-SE-03.c
+++ C/Exams/Input_Output/2016-SE-03.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <err.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdint.h>
 #include <sys/param.h>
 #include <stdint.h>
 
+int fds[3];
+
+void close_all(void);
 int compareUint32_t(const void* a, const void* b);
 void sortFile(const char* file_name);
 void split(int halfCount, int fd, const char* temp_file, const char* source_file);
@@ -15,6 +19,17 @@ int openReadSafe(const char* file_name);
 uint32_t readNumber(int fd, const char* file_name);
 void merge(const char* temp1, const char* temp2, int temp1Size, int temp2Size, const char* source_file);
 void writeSafe(int fd, uint32_t number, const char* file_name);
+
+void close_all(void) {
+    int errno_ = errno;
+    for (int i = 0; i < 3; i++) {
+        if (fds[i] >= 0) {
+            close(fds[i]);
+        }
+    }
+
+    errno = errno_;
+}
 
 int compareUint32_t(const void* a, const void* b) {
     const uint32_t* num1 = (const uint32_t*)a;
@@ -32,6 +47,7 @@ int compareUint32_t(const void* a, const void* b) {
 int openReadSafe(const char* file_name) {
     int fd;
     if (( fd = open(file_name, O_RDONLY)) == -1) {
+        close_all();
         err(10, "Error while opening file %s", file_name);
     }
 
@@ -41,6 +57,7 @@ int openReadSafe(const char* file_name) {
 uint32_t readNumber(int fd, const char* file_name) {
     uint32_t number;
     if (read(fd, &number, sizeof(number)) == -1) {
+        close_all();
         err(11, "Error while reading from file %s", file_name);
     }
 
@@ -49,47 +66,47 @@ uint32_t readNumber(int fd, const char* file_name) {
 
 void writeSafe(int fd, uint32_t number, const char* file_name) {
     if (write(fd, &number, sizeof(uint32_t)) == -1) {
+        close_all();
         err(10, "Error while writing to file %s", file_name);
     }
 }
 
 void merge(const char* temp1, const char* temp2, int temp1Size, int temp2Size, const char* source_file) {
-    int fd1 = openReadSafe(temp1);
-    int fd2 = openReadSafe(temp2);
-    int fd;
+    fds[1] = openReadSafe(temp1);
+    fds[2] = openReadSafe(temp2);
 
-    if (( fd = open(source_file, O_RDWR | O_TRUNC)) == -1) {
+    if (( fds[0] = open(source_file, O_RDWR | O_TRUNC)) == -1) {
         err(11, "Error while reading from file %s", source_file);
     }
 
-    uint32_t num1 = readNumber(fd1, temp1);
-    uint32_t num2 = readNumber(fd2, temp2);
+    uint32_t num1 = readNumber(fds[1], temp1);
+    uint32_t num2 = readNumber(fds[2], temp2);
     int counter = 0, minSize = MIN(temp1Size, temp2Size);
 
     while (counter < minSize) {
         if (num1 < num2) {
-            writeSafe(fd, num1, source_file);
+            writeSafe(fds[0], num1, source_file);
             dprintf(1, "%d\n", num1);
-            num1 = readNumber(fd1, temp1);
+            num1 = readNumber(fds[1], temp1);
         } else {
-            writeSafe(fd, num2, source_file);
+            writeSafe(fds[0], num2, source_file);
             dprintf(1, "%d\n", num2);
-            num2 = readNumber(fd2, temp2);
+            num2 = readNumber(fds[2], temp2);
         }
         counter++;
     }
 
     while (counter < temp1Size) {
-        writeSafe(fd, num1, source_file);
+        writeSafe(fds[0], num1, source_file);
         dprintf(1, "%d\n", num1);
-        num1 = readNumber(fd1, temp1);
+        num1 = readNumber(fds[1], temp1);
         counter++;
     }
 
     while (counter < temp2Size) {
-        writeSafe(fd, num2, source_file);
+        writeSafe(fds[0], num2, source_file);
         dprintf(1, "%d\n", num2);
-        num2 = readNumber(fd2, temp2);
+        num2 = readNumber(fds[2], temp2);
         counter++;
     }
 }
@@ -144,7 +161,11 @@ void sortFile(const char* file_name) {
     }
 
     int fd = openReadSafe(file_name);
-    const char *temp1 = "temp1.txt", *temp2 = "temp2.txt";
+    char temp1[11] = "tempXXXXXX", temp2[11] = "tempXXXXXX";
+    if (mkstemp(temp1) == -1 || mkstemp(temp2) == -1) {
+        err(5, "Could not mkstemp");
+    }
+
     int numbersCount = fileSize / sizeof(uint32_t);
     int halfCount = numbersCount / 2;
     int otherHalfCount = numbersCount - halfCount; // just in case it is and odd count
@@ -153,6 +174,11 @@ void sortFile(const char* file_name) {
     close(fd);
 
     merge(temp1, temp2, halfCount, otherHalfCount, file_name);
+    close_all();
+
+    if (unlink(temp1) == -1 || unlink(temp2) == -1) {
+        err(6, "Could not unlink");
+    }
 }
 
 int main(int argc, char** argv){
