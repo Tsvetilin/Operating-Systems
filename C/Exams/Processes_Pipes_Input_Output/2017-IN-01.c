99e309b19e3dea6bfc741c725e1f6b384c1a3bde
Stoyan Zlatev <tonkatazl@gmail.com>
2023-06-20 22:02:21 +0300

    Add C Exam Task Solution

--- C/Exams/Processes_Pipes_Input_Output/2017-IN-01.c
+++ C/Exams/Processes_Pipes_Input_Output/2017-IN-01.c
@@ -10,23 +10,23 @@
 int main(void) {
     int a[2];
 
-    if(pipe(a) == -1) {
+    if (pipe(a) == -1) {
         err(1, "pipe failed");
     }
 
     pid_t pid;
-    if((pid = fork()) == -1) {
+    if ((pid = fork()) == -1) {
         err(2, "first fork failed");
     }
 
-    if(pid == 0) {
+    if (pid == 0) {
         close(a[0]);
         //close(1);
         //dup(a[1]);
-        if(dup2(a[1], 1) == -1) {
+        if (dup2(a[1], 1) == -1) {
             err(3, "cut dup failed");
         }
-        if(execlp("cut", "cut", "-d:", "-f7", "/etc/passwd", (char *) NULL) == -1) {
+        if (execlp("cut", "cut", "-d:", "-f7", "/etc/passwd", (char *) NULL) == -1) {
             err(4, "cut exec failed");
         }
     } else {
@@ -34,20 +34,20 @@ int main(void) {
     }
 
     int b[2];
-    if(pipe(b) == -1) {
+    if (pipe(b) == -1) {
         err(4, "Second pipe failed");
     }
 
-    if((pid = fork()) == -1) {
+    if ((pid = fork()) == -1) {
         err(5, "sort fork failed");
     }
 
-    if(pid == 0) {
+    if (pid == 0) {
         close(b[0]);
-        if(dup2(a[0], 0) == -1) {
+        if (dup2(a[0], 0) == -1) {
             err(6, "error dup read end sort");
         }
-        if(dup2(b[1], 1) == -1) {
+        if (dup2(b[1], 1) == -1) {
             err(6, "error dup write end sort");
         }
         execlp("sort", "sort", (char *) NULL);
@@ -57,25 +57,25 @@ int main(void) {
 
     int c[2];
 
-    if(pipe(c) == -1){
+    if (pipe(c) == -1) {
         err(6, "error pipe for uniq");
     }
 
-    if((pid = fork()) == -1) {
+    if ((pid = fork()) == -1) {
         err(7, "error forking uniq");
     }
 
-    if(pid == 0) {
+    if (pid == 0) {
         close(c[0]);
 
-        if(dup2(b[0], 0) == -1) {
+        if (dup2(b[0], 0) == -1) {
             err(9, "error dup read end uniq");
         }
-        if(dup2(c[1], 1) == -1) {
+        if (dup2(c[1], 1) == -1) {
             err(10, "error dup read end uniq");
         }
 
-        if(execlp("uniq", "uniq", "-c", (char *)NULL) == -1) {
+        if (execlp("uniq", "uniq", "-c", (char *) NULL) == -1) {
             err(11, "error exec uniq");
         }
     } else {
@@ -85,13 +85,13 @@ int main(void) {
     close(a[0]);
     close(b[0]);
 
-    while(wait(NULL) > 0);
+    while (wait(NULL) > 0);
 
-    if(dup2(c[0], 0) == -1){
+    if (dup2(c[0], 0) == -1) {
         err(11, "error dup read end sort -n");
     }
 
-    if(execlp("sort", "sort", "-n", (char *)NULL) == -1){
+    if (execlp("sort", "sort", "-n", (char *) NULL) == -1) {
         err(6, "error exec sort -n");
     }
 
@@ -101,10 +101,12 @@ int main(void) {
 
 #v2
 
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <err.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 int ps[3][2];
 
@@ -112,6 +114,23 @@ void close_all(void);
 void dup2_safe(int fd1, int fd2);
 void pipe_safe(int fd_num);
 pid_t fork_safe(void);
+void wait_child(void);
+
+void wait_child(void) {
+    int status;
+    if (wait(&status) == -1) {
+        err(4, "Could not wait for child process to finish");
+    }
+
+    if (!WIFEXITED(status)) {
+        errx(5, "Child process did not terminate normally");
+    }
+
+    if (WEXITSTATUS(status) != 0) {
+        errx(6, "Child process finished with exit code not 0");
+    }
+}
+
 
 pid_t fork_safe(void) {
     pid_t child = fork();
@@ -155,17 +174,19 @@ void dup2_safe(int fd1, int fd2) {
 
 int main(void) {
     pipe_safe(0);
+
     pid_t child = fork_safe();
 
     if (child == 0) {
         close(ps[0][0]);
         dup2_safe(ps[0][1], 1);
-        if (execlp("cut", "cut", "-d", ":", "-f", "7", "/etc/passwd", (char*)NULL) == -1) {
+        if (execlp("cut", "cut", "-d", ":", "-f", "7", "/etc/passwd", (char *) NULL) == -1) {
             close_all();
             err(3, "Could not execlp cut");
         }
     }
 
+    wait_child();
     close(ps[0][1]);
     pipe_safe(1);
 
@@ -175,12 +196,13 @@ int main(void) {
         close(ps[1][0]);
         dup2_safe(ps[0][0], 0);
         dup2_safe(ps[1][1], 1);
-        if (execlp("sort", "sort", (char*)NULL) == -1) {
+        if (execlp("sort", "sort", (char *) NULL) == -1) {
             close_all();
             err(3, "Could not execlp sort");
         }
     }
 
+    wait_child();
     close(ps[1][1]);
     pipe_safe(2);
 
@@ -190,20 +212,22 @@ int main(void) {
         close(ps[2][0]);
         dup2_safe(ps[2][1], 1);
         dup2_safe(ps[1][0], 0);
-        if (execlp("uniq", "uniq", "-c", (char*)NULL) == -1) {
+        if (execlp("uniq", "uniq", "-c", (char *) NULL) == -1) {
             close_all();
             err(3, "Could not execlp uniq");
         }
     }
 
+    wait_child();
     close(ps[2][1]);
     dup2_safe(ps[2][0], 0);
     close_all();
 
-    if (execlp("sort", "sort", "-n", (char*)NULL) == -1) {
+    if (execlp("sort", "sort", "-n", (char *) NULL) == -1) {
         err(3, "Could not sort");
     }
 }
+
 //Напишете програма на C, която използвайки външни shell команди през pipe() да
 //        извежда статистика за броя на използване на различните shell-ове от потребителите, дефинирани
 //        в системата. Изходът да бъде сортиран във възходящ ред според брой използвания на shell-овете.
