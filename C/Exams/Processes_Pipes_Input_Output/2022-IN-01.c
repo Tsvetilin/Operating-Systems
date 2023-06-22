a51a3ccea67475173db9d1cad2c7b27057919797
Stoyan Zlatev <tonkatazl@gmail.com>
2023-06-22 21:49:20 +0300

    Add C Exam Task

--- C/Exams/Processes_Pipes_Input_Output/2022-IN-01.c
+++ C/Exams/Processes_Pipes_Input_Output/2022-IN-01.c
@@ -1,3 +1,155 @@
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int pipes_count = 2;
+int pds[2][2];
+
+void check_args(const char* argv1, const char* argv2, const char* program);
+void write_safe(int fd, const char* str);
+void pipe_safe(int pipe_ind);
+void wait_child(void);
+void handle_errx(int exit_code, const char* message);
+void handle_err(int exit_code, const char* message);
+void close_all(void);
+pid_t fork_safe(void);
+void create_pipes(void);
+void read_pipe(int fd);
+
+void read_pipe(int fd) {
+    char c;
+    if (read(fd, &c, sizeof(c)) == -1) {
+        handle_err(9, "Could not read from pipe");
+    }
+}
+
+void create_pipes(void) {
+    for (int i = 0; i < pipes_count; i++) {
+        pipe_safe(i);
+    }
+}
+
+void close_all(void) {
+    int errno_ = errno;
+    for (int i = 0; i < pipes_count; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (pds[i][j] >= 0) {
+                close(pds[i][j]);
+            }
+        }
+    }
+
+    errno = errno_;
+}
+
+void handle_errx(int exit_code, const char* message) {
+    close_all();
+    errx(exit_code, message);
+}
+void handle_err(int exit_code, const char* message) {
+    close_all();
+    err(exit_code, message);
+}
+
+void wait_child(void) {
+    int status;
+    if (wait(&status) == -1) {
+        handle_err(6, "Could not wait for child process to finish");
+    }
+
+    if (!WIFEXITED(status)) {
+        handle_errx(7, "Child process did not terminate normally");
+    }
+
+    if (WEXITSTATUS(status) != 0) {
+        handle_errx(8, "Child process finished with exit code not 0");
+    }
+}
+
+void pipe_safe(int pipe_ind) {
+    if (pipe(pds[pipe_ind]) == -1) {
+        handle_err(5, "Could not pipe");
+    }
+}
+
+pid_t fork_safe(void) {
+    const pid_t child = fork();
+    if (child == -1) {
+        handle_err(4, "Could not fork\n");
+    }
+
+    return child;
+}
+
+void write_safe(int fd, const char* str) {
+    int str_length = strlen(str);
+    if (write(fd, str, str_length) != str_length) {
+        handle_err(3, "Could not write to stdout");
+    }
+}
+
+void check_args(const char* argv1, const char* argv2, const char* program) {
+    if (strlen(argv1) != 1 || strlen(argv2) != 1) {
+        errx(2, "Invalid arguments. Usage %s N D", program);
+    }
+
+    char num1 = argv1[0], num2 = argv2[0];
+
+    if (!(num1 <= '9' && num1 >= '0') || !(num2 <= '9' && num2 >= '0')) {
+        errx(2, "Invalid arguments. Usage %s N D", program);
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        errx(1, "Invalid arguments. Usage %s N D", argv[0]);
+    }
+
+    check_args(argv[1], argv[2], argv[0]);
+    int N = (int)argv[1][0] - '0', D = (int)argv[2][0] - '0';
+    create_pipes();
+    const pid_t child = fork_safe();
+
+    if (child == 0) {
+        close(pds[1][0]);
+        close(pds[0][1]);
+    } else {
+        close(pds[0][0]);
+        close(pds[1][1]);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (child == 0) {
+            read_pipe(pds[0][0]);
+            write_safe(1, "DONG\n");
+            write_safe(pds[1][1], "0");
+            if (i == N) {
+                exit(0);
+            }
+        } else {
+            write_safe(1, "DING\n");
+            write_safe(pds[0][1], "1");
+            read_pipe(pds[1][0]);
+            sleep(D);
+        }
+    }
+
+    wait_child();
+    close_all();
+    exit(0);
+}
+
+//v2 does not match the given algorithm!!!
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <err.h>
@@ -68,165 +220,6 @@ int main(int argc, char** argv) {
     exit(0);
 }
 
-#v2
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <err.h>
-#include <string.h>
-#include <stdint.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <fcntl.h>
-
-char temp[5] = "temp";
-
-void check_args(const char* argv1, const char* argv2, const char* program);
-void write_safe(const char* str);
-void wait_signal(const char* fifo);
-void signal_s(const char* fifo);
-void mkfifo_safe(const char* fifo);
-void write_pipe(int fd, char c);
-void read_pipe(int fd);
-void unlink_safe(const char* file_name);
-void wait_child(void);
-void handle_errx(int exit_code, const char* message);
-void handle_err(int exit_code, const char* message);
-
-void handle_errx(int exit_code, const char* message) {
-    unlink_safe(temp);
-    errx(exit_code, message);
-}
-void handle_err(int exit_code, const char* message) {
-    unlink_safe(temp);
-    err(exit_code, message);
-}
-
-void wait_child(void) {
-    int status;
-    if (wait(&status) == -1) {
-        handle_err(12, "Could not wait for child process to finish");
-    }
-
-    if (!WIFEXITED(status)) {
-        handle_errx(13, "Child process did not terminate normally");
-    }
-
-    if (WEXITSTATUS(status) != 0) {
-        handle_errx(14, "Child process finished with exit code not 0");
-    }
-}
-
-void unlink_safe(const char* file_name) {
-    if (unlink(file_name) == -1) {
-        err(11, "Could not remove file %s", file_name);
-    }
-}
-
-void read_pipe(int fd) {
-    char c;
-    int bytes_read;
-    if ((bytes_read = read(fd, &c, sizeof(c))) != sizeof(c)) {
-        if (bytes_read == -1) {
-            handle_err(9, "Could not read from pipe");
-        } else {
-            handle_errx(9, "Could not read from pipe");
-        }
-    }
-}
-
-void write_pipe(int fd, char c) {
-    int bytes_written;
-    if ((bytes_written = write(fd, &c, sizeof(c))) != sizeof(c)) {
-        if (bytes_written == -1) {
-            handle_err(10, "Could not read from pipe");
-        } else {
-            handle_errx(10, "Could not read from pipe");
-        }
-    }
-}
-void mkfifo_safe(const char* fifo) {
-    if (mkfifo(fifo, 0666) == -1) {
-        err(9, "Could not mkfifo");
-    }
-}
-void signal_s(const char* fifo) {
-    int fd = open(fifo, O_WRONLY);
-    char c = '1';
-    write_pipe(fd, c);
-
-    close(fd);
-}
-
-void wait_signal(const char* fifo) {
-    int fd = open(fifo, O_RDONLY);
-    read_pipe(fd);
-    close(fd);
-}
-
-pid_t fork_safe(void);
-
-pid_t fork_safe(void) {
-    const pid_t child = fork();
-    if (child == -1) {
-        handle_err(3, "Could not fork\n");
-    }
-
-    return child;
-}
-
-void write_safe(const char* str) {
-    int str_length = strlen(str);
-    if (write(1, str, str_length) != str_length) {
-        handle_err(2, "Could not write to stdout");
-    }
-}
-
-void check_args(const char* argv1, const char* argv2, const char* program) {
-    if (strlen(argv1) != 1 || strlen(argv2) != 1) {
-        errx(1, "Invalid arguments. Usage %s N D", program);
-    }
-
-    char num1 = argv1[0], num2 = argv2[0];
-
-    if (!(num1 <= '9' && num1 >= '0') || !(num2 <= '9' && num2 >= '0')) {
-        errx(1, "Invalid arguments. Usage %s N D", program);
-    }
-}
-
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        errx(1, "Invalid arguments. Usage %s N D", argv[0]);
-    }
-
-    check_args(argv[1], argv[2], argv[0]);
-
-    int N = (int)argv[1][0] - '0', D = (int)argv[2][0] - '0';
-    mkfifo_safe(temp);
-
-    const pid_t child = fork_safe();
-    for (int i = 1; i <= N; i++) {
-        if (child == 0) {
-            wait_signal(temp);
-            write_safe("DONG\n");
-            signal_s(temp);
-            if (i == N) {
-                exit(0);
-            }
-        } else {
-            write_safe("DING\n");
-            signal_s(temp);
-            wait_signal(temp);
-            sleep(D);
-        }
-    }
-
-    wait_child();
-    unlink_safe(temp);
-    exit(0);
-}
 
 //Ваши колеги - асистенти по ОС имат нужда от демострационна програма на C, която
 //да служи като пример за конкурентност и синхронизация на процеси. Напишете такава програма,
